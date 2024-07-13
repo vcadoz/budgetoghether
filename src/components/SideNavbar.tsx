92a007306051e72534287510c345addbc3734bc3
vcadoz <vincent.cadoz@gmail.com>
2024-07-13 21:51:32 +0200

    ADD login and register with dashboard template

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -1,3 +1,4 @@
+/** @format */
 "use client";
 
 import { useState } from "react";
--- test.py
+++ test.py
@@ -9,7 +9,7 @@ headers = {
     "Authorization" : "Bearer 18ee7bd53d102260202f6a63c1662d3ea1c09f2f-3910f79b-37b1-4cf1-88e4-0752fe1cbf62"
 }
 
-response = requests.get(url, headers=headers)
+response = requests.post(url, headers=headers)
 
 print(f"Status Code: {response.status_code}")
 print(f"Response Body: {response.text}")
